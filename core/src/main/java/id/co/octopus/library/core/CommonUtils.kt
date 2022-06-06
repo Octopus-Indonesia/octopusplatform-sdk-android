8127190a11b9493b122be4d1045a63f8a5278f26
muasdev <muasdev@gmail.com>
2022-06-06 22:37:14 +0800

    add method limit decimal for edittext

--- core/src/main/java/id/co/octopus/library/core/CommonUtils.kt
+++ core/src/main/java/id/co/octopus/library/core/CommonUtils.kt
@@ -7,6 +7,9 @@ import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import kotlin.math.ceil
@@ -108,5 +111,68 @@ object CommonUtils {
             quantity = (ceil(qty * 2) / 2)
         }
         return quantity
+    }
+
+    //limit decimal
+    fun EditText.addDecimalLimiter(maxLimit: Int = 1) {
+
+        this.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable?) {
+                val str = this@addDecimalLimiter.text!!.toString()
+                if (str.isEmpty()) return
+                val str2 = decimalLimiter(str, maxLimit)
+
+                if (str2 != str) {
+                    this@addDecimalLimiter.setText(str2)
+                    val pos = this@addDecimalLimiter.text!!.length
+                    this@addDecimalLimiter.setSelection(pos)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
+    }
+
+    fun EditText.decimalLimiter(string: String, MAX_DECIMAL: Int): String {
+
+        var str = string
+        if (str[0] == '.') str = "0$str"
+        val max = str.length
+
+        var rFinal = ""
+        var after = false
+        var i = 0
+        var up = 0
+        var decimal = 0
+        var t: Char
+
+        val decimalCount = str.count{ ".".contains(it) }
+
+        if (decimalCount > 1)
+            return str.dropLast(1)
+
+        while (i < max) {
+            t = str[i]
+            if (t != '.' && !after) {
+                up++
+            } else if (t == '.') {
+                after = true
+            } else {
+                decimal++
+                if (decimal > MAX_DECIMAL)
+                    return rFinal
+            }
+            rFinal += t
+            i++
+        }
+        return rFinal
     }
 }
